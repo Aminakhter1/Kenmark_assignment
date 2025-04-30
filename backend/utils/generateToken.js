2aa9557b3f4c2723ec566c7b6334423f4ec7d9ff
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2025-05-01 02:11:00 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,9 +1,9 @@
 
 // utils/generateToken.js
 const jwt = require('jsonwebtoken');
-
+const JWT_SECRET="AMINAKHTER";
 const generateToken = (userId, role) => {
-  return jwt.sign({ userId, role }, process.env.JWT_SECRET, {
+  return jwt.sign({ userId, role },JWT_SECRET, {
     expiresIn: '7d',
   });
 };
