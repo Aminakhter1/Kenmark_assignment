2108d7b2ba39ca4063b53519973c6fb50f18f568
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2025-05-01 02:10:00 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 
 // middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
-
+const JWT_SECRET="AMINAKHTER";
 const protect = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
@@ -10,7 +10,7 @@ const protect = (req, res, next) => {
 
   const token = authHeader.split(' ')[1];
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token,JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
