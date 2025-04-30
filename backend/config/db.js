abd01e2894d283855d668c24d180f8ef54f86a99
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2025-05-01 02:06:08 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://aminakhter1166:iKC3ZCtHscD3ahXu@cluster0.nhwmuoj.mongodb.net/psquare,");
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
